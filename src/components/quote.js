dd4b83fc50fdc48dfba263f255ea3b48e296f3cd
hayatu <hbawazy@gmail.ccom>
2019-09-20 07:33:23 +0100

    still couldnt figure out changing next quote

--- src/components/quote.js
+++ src/components/quote.js
@@ -6,29 +6,54 @@ import { Icon } from 'antd';
 export class quote extends Component {
     constructor(props){
         super(props);
+        // this.handleClick = this.handleClick.bind(this);
         this.state={
             qoutes:[
              {
+                 id:1,
+                quote:'the World is a very funny place it kicks you when you are down but staying down is not an option',
+                author:'Hayatu B. Waziri'
+            },
+            {
+                id:2,
+                quote:'the World is a very funny place it kicks you when you are down but staying down is not an option',
+                author:'Hayatu B. Waziri'
+            },
+            {
+                id:3,
+                quote:'the World is a very funny place it kicks you when you are down but staying down is not an option',
+                author:'Hayatu B. Waziri'
+            },
+            {
+                id:4,
                 quote:'the World is a very funny place it kicks you when you are down but staying down is not an option',
                 author:'Hayatu B. Waziri'
             },
         ]
-        }
+        }   
+
     }
+
+    handleClick(e){
+        
+       
+       console.log(e);
+    }
+
+
     render() {
         return (
             <div style={{textAlign:"center",width:"30%",display:"block",marginLeft:"auto",marginRight:"auto" }}>
                 
-                {this.state.qoutes.map(quote=>{
-                    return <div>
-                        <p>"
-                            <h3>{quote.quote}</h3>
-                            "</p> 
-                            <h5>--{quote.author}</h5>
-                              </div>})}
+                {
+                <div>
+                    <p>"{this.state.qoutes[0].quote}"</p>  
+                    <h5>--{this.state.qoutes[0].author}</h5>
+                 </div> 
+                }
 
                 
-                <button><Icon type="sync" /></button>  
+                <button><Icon type="sync" onClick={this.handleClick}/></button>  
                 <button><Icon type="heart" /></button>
             </div>
         )
