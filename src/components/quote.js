c83bb165318baeb77a6c272c1e1e4c7a102dddfd
hayatu <hbawazy@gmail.ccom>
2019-10-07 23:01:17 +0100

    figured how to display just one quote

--- src/components/quote.js
+++ src/components/quote.js
@@ -35,10 +35,10 @@ export class quote extends Component {
 
     render() {
         let {count, qoutes }=this.state;
-        // const rand = quotes[Math.floor(Math.random() * quotes.length)];
+        const rand = Math.floor(Math.random() * qoutes.length);
 
         const dispQ = qoutes.map(quote=>{
-                        return <div key={quote.id}><p>"{quote.body}"</p> 
+                        return <div key={quote.id}><p><span>"</span>{quote.body}<span>"</span></p> 
                                     <h2>--{quote.author}</h2> 
                                </div>
                     })
@@ -46,9 +46,13 @@ export class quote extends Component {
         return (
             <div style={{textAlign:"center",width:"30%",display:"block",marginLeft:"auto",marginRight:"auto" }}>
                 
-                {dispQ}
+               {<div>
+                   {/* <h4>{qoutes[rand].body}</h4>
+                    <h2>{qoutes[rand].author}</h2> */}
+                    {dispQ[rand]}
+                    </div>
+               }
 
-                
                 <button><Icon type="sync" onClick={this.handleClick}/></button>  
                 <button><Icon type="heart" /></button>
             </div>
